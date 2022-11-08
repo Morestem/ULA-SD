ce0e63465f0c5b5758c4d8eedecc6639b65019b7
Morestem <117694152+Morestem@users.noreply.github.com>
2022-11-07 22:38:30 -0300

    Update dobro.vhd

--- dobro.vhd
+++ dobro.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity dobro is
-    Port ( q : in  STD_LOGIC_VECTOR (3 downto 0); -- recebe uma entrada de 4 bits e realiza um deslocamento para a esquerda
+    Port ( q : in  STD_LOGIC_VECTOR (3 downto 0); -- recebe uma entrada de 4 bits
 	 resdobro : out STD_LOGIC_VECTOR (3 downto 0);
 	 extra : out STD_LOGIC; -- saída para o bit mais significativo após o deslocamento
 	 zeroflagdobroc : out STD_LOGIC); -- gera, como saídas, o dobro do valor de entrada e sua flag
