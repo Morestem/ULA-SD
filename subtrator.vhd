39a6d1a8b1d62ee9af640eb01a63e83ecb590d06
Morestem <117694152+Morestem@users.noreply.github.com>
2022-11-08 21:21:45 -0300

    Update subtrator.vhd

--- subtrator.vhd
+++ subtrator.vhd
@@ -26,19 +26,16 @@ end component;
 signal coutaux : STD_LOGIC_VECTOR (3 downto 0); -- sinal de carry auxiliar
 signal TMP : STD_LOGIC_VECTOR (3 downto 0); -- sinal auxiliar para a realização das contas
 signal resaux : STD_LOGIC_VECTOR (3 downto 0); -- sinal auxiliar do resultado
-signal sinalflg : STD_LOGIC; -- sinal para a indicação da flag de sinal
-signal garbage : STD_LOGIC; -- sinal para a realização da quinta soma, com intuito de facilitar a verificação do sinal
 
 begin
 TMP <= not bvec; -- a segunda entrada é invertida
--- aqui, são realizadas cinco somas com um carry in inicial no valor '1', para que seja realizado o complemento a dois da segunda entrada
+-- aqui, são realizadas 4 somas com um carry in inicial no valor '1', para que se tenha o complemento a dois da segunda entrada
 s0 : mod_adder port map (avec(0),TMP(0),'1',coutaux(0),resaux(0));
 s1 : mod_adder port map (avec(1),TMP(1),coutaux(0),coutaux(1),resaux(1));
 s2 : mod_adder port map (avec(2),TMP(2),coutaux(1),coutaux(2),resaux(2));
 s3 : mod_adder port map (avec(3),TMP(3),coutaux(2),coutaux(3),resaux(3));
-s4 : mod_adder port map ('0','1',coutaux(3),sinalflg,garbage);
 res <= resaux;
-signalflagsu <= not sinalflg;
+signalflagsu <= resaux(3); -- a flag de sinal é definida pelo bit mais significativo
 couts <= coutaux(3);
 overflows <= coutaux(3) xor coutaux(2);
 zeroflag <= (resaux(0) NOR resaux(1)) AND (resaux(2) NOR resaux(3));
