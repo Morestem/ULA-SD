c1e6be6e13b291ae6034dedf7975e0dab48a6ccb
Morestem <117694152+Morestem@users.noreply.github.com>
2022-12-02 22:15:31 -0300

    Update AND.vhd

--- AND.vhd
+++ AND.vhd
@@ -5,7 +5,8 @@ entity full_and is
     Port ( s : in  STD_LOGIC_VECTOR (3 downto 0);
            d : in  STD_LOGIC_VECTOR (3 downto 0); -- recebe duas entradas de 4 bits, que serão comparados com a porta lógica AND
            asd : out  STD_LOGIC_VECTOR (3 downto 0);
-           zeroflagandc : out STD_LOGIC); -- gera, como saída, o vetor da comparação das entradas e sua flag de zero, em caso de resulato igual a "0000"
+           zeroflagandc : out STD_LOGIC); -- gera, como saída, o vetor da comparação das entradas e sua flag de zero, em caso de resultado
+ igual a "0000"
 end full_and;
 
 architecture Behavioral of full_and is
